1bcf913a1accdc5465a1eaeba6ddbec640e04b00
Mark Thebault <mark.thebault@cloudreach.com>
2019-04-01 18:58:56 +0200

    upload dataset in landing bucket

---
+++ terraform/modules/s3-dlk-buckets/main.tf
@@ -0,0 +1,20 @@
+variable "names" {
+  type = "list"
+}
+
+resource "aws_s3_bucket" "dlk" {
+  count = "${length(var.names)}"
+
+  bucket = "${element(var.names, count.index)}"
+  acl    = "private"
+
+  tags = {
+    Name        = "Datalake Buckets"
+    Environment = "Dev"
+    Terraform   = "true"
+  }
+}
+
+output "s3_bucket_id" {
+  value = "${aws_s3_bucket.dlk.*.id}"
+}
